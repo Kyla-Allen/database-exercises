2a44c8c90b96675e0189aa8470151fafc95da968
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-11-30 14:35:54 -0600

    fixed exercise 3

--- update_exercises.sql
+++ update_exercises.sql
@@ -3,23 +3,29 @@ USE codeup_test_dp;
 SELECT 'All albums' as 'Caption';
 Select name from albums;
 Update albums
-  set sales = sales * 10,
-  release_date = release_date - 100 WHERE release_date < 1980,
-  artist = 'Peter Jackson' where artist = 'Michael Jackson';
+  set sales = sales * 10;
+Update albums
+  set release_date = release_date - 100 WHERE release_date < 1980;
+Update albums
+  set artist = 'Peter Jackson' where artist = 'Michael Jackson';
 Select * from albums;
 
 select 'all albums released before 1980' as 'caption';
 select name from albums where release_date < 1980;
 Update albums
-  set sales = sales * 10,
-  release_date = release_date - 100 WHERE release_date < 1980,
-  artist = 'Peter Jackson' where artist = 'Michael Jackson';
+  set sales = sales * 10;
+Update albums
+  set release_date = release_date - 100 WHERE release_date < 1980;
+Update albums
+  set artist = 'Peter Jackson' where artist = 'Michael Jackson';
 Select * from albums;
 
 select 'all albums by Michael Jackson' as 'caption';
 select name from albums where artist = 'Michael Jackson';
 Update albums
-  set sales = sales * 10,
-  release_date = release_date - 100 WHERE release_date < 1980,
-  artist = 'Peter Jackson' where artist = 'Michael Jackson';
+  set sales = sales * 10;
+Update albums
+  set release_date = release_date - 100 WHERE release_date < 1980;
+Update albums
+  set artist = 'Peter Jackson' where artist = 'Michael Jackson';
 Select * from albums;
