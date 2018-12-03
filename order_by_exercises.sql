714b9f0fd82916731394fea59cdd8c2663e9b6c0
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-12-03 12:31:04 -0600

    finished the order by exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,14 +1,10 @@
 USE employees;
 
-select * from employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
-select * from employees where last_name LIKE 'E%';
-select * from employees where hire_date BETWEEN '1989-12-31' and '2001-01-01';
-select * from employees where birth_date LIKE '%-12-25';
-select * from employees where last_name Like '%q%';
+select * from employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') Order by last_name DESC, first_name DESC;
+select * from employees where last_name LIKE 'E%' ORDER BY emp_no DESC;
+select * from employees where hire_date BETWEEN '1989-12-31' and '2001-01-01' AND birth_date like '%-12-25' ORDER BY birth_date DESC, hire_date DESC;
 
 
-select * from employees where (first_name = 'Irena' OR first_name = 'Vidya' oR first_name = 'maya') and gender = 'M';
-select * from employees where last_name like 'e%' or last_name like '%e';
-select * from employees where last_name like 'e%' and last_name like '%e';
-select * from employees where hire_date BETWEEN '1989-12-31' and '2001-01-01' AND birth_date like '%-12-25';
-select * from employees where last_name like '%q%' AND last_name not like '%qu%';
+
+
+
