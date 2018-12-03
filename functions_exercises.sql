746125e57494cc611e8b8f011d1fe1d2a823d923
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-12-03 15:31:07 -0600

    finished the functione xercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,8 +1,11 @@
 USE employees;
 
-select * from employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') Order by last_name DESC, first_name DESC;
 select concat(first_name, ' ', last_name) from employees where last_name LIKE 'E%' ORDER BY emp_no DESC ;
-select * from employees where hire_date BETWEEN '1989-12-31' and '2001-01-01' AND birth_date like '%-12-25' ORDER BY birth_date, hire_date DESC;
+
+
+select datediff(current_date (), hire_date) from employees where hire_date
+BETWEEN '1989-12-31' and '2001-01-01' AND birth_date like '%-12-25'
+ORDER BY birth_date, hire_date DESC;
 
 
 
