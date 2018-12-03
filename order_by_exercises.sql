42fdc5cd138d2319690097dc284d3e896fcf2574
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-12-03 13:37:05 -0600

    fixed an error in the order by exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,7 +2,7 @@ USE employees;
 
 select * from employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') Order by last_name DESC, first_name DESC;
 select * from employees where last_name LIKE 'E%' ORDER BY emp_no DESC;
-select * from employees where hire_date BETWEEN '1989-12-31' and '2001-01-01' AND birth_date like '%-12-25' ORDER BY birth_date DESC, hire_date DESC;
+select * from employees where hire_date BETWEEN '1989-12-31' and '2001-01-01' AND birth_date like '%-12-25' ORDER BY birth_date, hire_date DESC;
 
 
 
