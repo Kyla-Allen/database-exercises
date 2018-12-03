eb6f1a212f4cca7b5a6f82d49faeba7cafa05817
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-12-03 13:52:01 -0600

    finished the limit exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,4 +1,4 @@
 USE employees;
 
 Select Distinct last_name from employees ORDER BY last_name DESC LIMIT 10;
-select * from employees where hire_date BETWEEN '1989-12-31' and '2001-01-01' AND birth_date like '%-12-25' ORDER BY birth_date, hire_date DESC LIMIT 5;
+select * from employees where hire_date BETWEEN '1989-12-31' and '2001-01-01' AND birth_date like '%-12-25' ORDER BY birth_date, hire_date DESC LIMIT 5 OFFSET 45;
