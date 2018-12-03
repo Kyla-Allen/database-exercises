8b17f65de12b767bad6b1ef488f714e21b61f89c
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-12-03 11:14:29 -0600

    finished the where clause exercises. need to go fix a couple so im doing that

--- where_exercises.sql
+++ where_exercises.sql
@@ -7,4 +7,8 @@ select * from employees where birth_date LIKE '%-12-25';
 select * from employees where last_name Like '%q%';
 
 
-select * from employees where first_name = 'Irena' OR first_name = 'Vidya' oR first_name = 'maya' and gender = 'M';
+select * from employees where (first_name = 'Irena' OR first_name = 'Vidya' oR first_name = 'maya') and gender = 'M';\
+select * from employees where last_name like 'e%' or last_name like '%e';
+select * from employees where last_name like 'e%' and last_name like '%e';
+select * from employees where hire_date BETWEEN 1899 and 2001 AND birth_date like '%-12-25';--FIX THIS TOO
+select * from employees where last_name like '%q%' AND last_name not like '%qu%';
