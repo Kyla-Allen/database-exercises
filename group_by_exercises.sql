dcdcb4cc4b7fdb6defa495e1527f4cda7c730d5e
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2018-12-04 10:24:15 -0600

    finished the group_by exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -16,32 +16,53 @@ SELECT last_name from employees
  WHERE last_name like 'E%'
  AND last_name like '%e'
  GROUP BY last_name;
--- Eldridge
--- Erbe
--- Erde
--- Erie
--- Etalle
 
 
 
--- Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
+
+-- Update your previous query to now find unique combinations of first and last name
+--  where the last name starts and ends with 'E'. You should get 846 rows.
 --
+SELECT last_name from employees
+ WHERE last_name like 'E%'
+ AND last_name like '%e'
+ GROUP BY first_name, last_name;
+
+
+
+
+
+
+
+
+
 -- Find the unique last names with a 'q' but not 'qu'. Your results should be:
 --
---
--- Chleq
--- Lindqvist
--- Qiwen
+
+SELECT last_name from employees
+WHERE last_name like '%q%'
+AND last_name not like '%qu%'
+GROUP BY last_name;
+
 
 
 
 -- Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
 --
+SELECT last_name, Count(last_name) from employees
+WHERE last_name like '%q%'
+AND last_name not like '%qu%'
+GROUP BY last_name;
 
 
 
--- Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names. Your results should be:
---
---
+-- Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*)
+-- and GROUP BY to find the number of employees for
+-- each gender with those names. Your results should be:
 -- 441 M
 -- 268 F
+
+SELECT count(*), gender from employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender
+ORDER BY gender;
